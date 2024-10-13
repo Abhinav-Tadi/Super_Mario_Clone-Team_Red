1300595796a0041c1b538441de802680d2b2b44f
Abhinav Tadi <tadi.abhinav2324@gmail.com>
2024-10-13 22:32:03 +0530

    Made Final Optimisations

--- Source/Headers/Global.hpp
+++ Source/Headers/Global.hpp
@@ -3,23 +3,20 @@
 constexpr float BRICK_PARTICLE_SPEED = 4;
 //A coin jumping out of the question block.
 constexpr float COIN_JUMP_SPEED = -6;
-constexpr float GOOMBA_SPEED = 1;
+constexpr float GOOMBA_SPEED = 0;
 constexpr float GRAVITY = 0.25f;
 constexpr float KOOPA_SHELL_SPEED = 4;
-constexpr float KOOPA_SPEED = 1;
+constexpr float KOOPA_SPEED = 0;
 constexpr float MARIO_ACCELERATION = 0.25f;
 constexpr float MARIO_JUMP_SPEED = -4;
 constexpr float MARIO_WALK_SPEED = 2;
 constexpr float MAX_VERTICAL_SPEED = 8;
-constexpr float MUSHROOM_SPEED = 1;
+constexpr float MUSHROOM_SPEED = 0;
 
 constexpr unsigned char CELL_SIZE = 16;
 //We use this constant when Mario is growing or when he's invincible.
 constexpr unsigned char MARIO_BLINKING = 4;
-//Lemme explain:
-//1) Mario gets hit by an enemy.
-//2) Mario looks at you like "Oh no! Me lost! Mama mia!" and pauses for this amount.
-//3) Mario bounces off the screen.
+
 constexpr unsigned char MARIO_DEATH_DURATION = 32;
 constexpr unsigned char MARIO_GROWTH_DURATION = 64;
 constexpr unsigned char MARIO_INVINCIBILITY_DURATION = 128;
@@ -32,19 +29,18 @@ constexpr unsigned short COIN_ANIMATION_SPEED = 4;
 constexpr unsigned short ENTITY_UPDATE_AREA = 64;
 constexpr unsigned short GOOMBA_DEATH_DURATION = 32;
 //Why do I have so many animation speed constants? I use them only once.
-constexpr unsigned short GOOMBA_WALK_ANIMATION_SPEED = 32;
+constexpr unsigned short GOOMBA_WALK_ANIMATION_SPEED = 0;
 constexpr unsigned short KOOPA_GET_OUT_ANIMATION_SPEED = 4;
 constexpr unsigned short KOOPA_GET_OUT_DURATION = 512;
-constexpr unsigned short KOOPA_WALK_ANIMATION_SPEED = 32;
+constexpr unsigned short KOOPA_WALK_ANIMATION_SPEED = 0;
 constexpr unsigned short MARIO_WALK_ANIMATION_SPEED = 4;
-constexpr unsigned short QUESTION_BLOCK_ANIMATION_SPEED = 8;
+constexpr unsigned short QUESTION_BLOCK_ANIMATION_SPEED = 0;
 constexpr unsigned short SCREEN_HEIGHT = 240;
 constexpr unsigned short SCREEN_WIDTH = 320;
 
 constexpr std::chrono::microseconds FRAME_DURATION(16667);
 
-enum Cell
-{
+enum Cell{
 	ActivatedQuestionBlock,
 	Brick,
 	Coin,
@@ -54,10 +50,7 @@ enum Cell
 	Wall
 };
 
-//We use this for brick particles.
-//Damn, I should've called it "Particle"! I'm so dumb!
-struct Object
-{
+struct Object{
 	float horizontal_speed;
 	float vertical_speed;
 	float x;
