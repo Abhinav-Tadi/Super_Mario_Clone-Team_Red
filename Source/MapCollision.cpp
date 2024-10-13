91167427e09ba1194e5562ec3647c84b3e57a9db
Abhinav Tadi <tadi.abhinav2324@gmail.com>
2024-10-13 17:10:25 +0530

    Add functionality to MapCollision

--- Source/MapCollision.cpp
+++ Source/MapCollision.cpp
@@ -6,4 +6,63 @@
 
 #include "Headers/Global.hpp"
 #include "Headers/MapCollision.hpp"
+using namespace std;
 
+unsigned char map_collision(float i_x, float i_y, const vector<Cell>& i_check_cells, const Map& i_map){
+	float cell_x = i_x / CELL_SIZE;
+	float cell_y = i_y / CELL_SIZE;
+
+	unsigned char output = 0;
+
+	for (unsigned char a = 0; a < 4; a++){
+		short x;
+		short y;
+
+		switch (a){
+			case 0: //Top left cell
+			{
+				x = floor(cell_x);
+				y = floor(cell_y);
+
+				break;
+			}
+			case 1: //Top right cell
+			{
+				x = ceil(cell_x);
+				y = floor(cell_y);
+
+				break;
+			}
+			case 2: //Bottom left cell
+			{
+				x = floor(cell_x);
+				y = ceil(cell_y);
+
+				break;
+			}
+			case 3: //Bottom right cell
+			{
+				x = ceil(cell_x);
+				y = ceil(cell_y);
+			}
+		}
+
+		if (0 <= x && x < i_map.size()){
+			if (0 <= y && y < i_map[0].size()){
+				//Checking if the cell's type is one of the cell types we need to check.
+				if (i_check_cells.end() != find(i_check_cells.begin(), i_check_cells.end(), i_map[x][y]))
+				{
+					//In binary this will be 0001, 0010, 0100 or 1000.
+					output += pow(2, a);
+				}
+			}
+		}
+		//Assuming that the map borders are walls.
+		else if (i_check_cells.end() != find(i_check_cells.begin(), i_check_cells.end(), Cell::Wall))
+		{
+			output += pow(2, a);
+		}
+	}
+
+	return output;
+}
